dc2976ee6fcbc1ddfa8c8b0d476860c84c448fd0
optimistiks <mail@potapovmax.com>
2015-08-04 09:16:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+global.jQuery = require('jquery');
+global.Handlebars = require('handlebars');
+global.i18n = require('i18next-client');
+
+require('jquery-slimscroll');
+require('bootstrap');
+
+jQuery(function () {
+  console.log(jQuery.fn.slimscroll);
+});
