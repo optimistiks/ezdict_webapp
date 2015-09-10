2ced90586dcc3bd4bd9830cddeb47313bac939f7
Potapov Max <mpmsp@yandex-team.ru>
2015-09-10 18:54:13 +0300

    add meaning subform (#6)

--- src/card/components/CardFormMeaningsList/CardFormMeaningsList.jsx
+++ src/card/components/CardFormMeaningsList/CardFormMeaningsList.jsx
@@ -19,26 +19,33 @@ module.exports = React.createClass({
 
     render: function () {
 
-        var meaningsList = null;
+        var meaningNodes = null;
 
         if (this.props.meanings.length) {
-            let meaningNodes = this.props.meanings.map(function (meaning, index) {
+            meaningNodes = this.props.meanings.map(function (meaning, index) {
                 var boundClick = this.handleClick.bind(this, index);
                 return (
                     <a href="#" className="list-group-item" onClick={boundClick}>{meaning.text}</a>
                 );
             }.bind(this));
 
-            meaningsList = (
-                <div className="form-group">
-                    <label htmlFor="text">Значения</label>
-                    <ul className="list-group">
-                        {meaningNodes}
-                    </ul>
-                </div>
-            );
         }
 
-        return meaningsList;
+        return (
+          <div className="form-group">
+            <label htmlFor="text">Значения</label>
+            <ul className="list-group">
+              {meaningNodes}
+              <li className="list-group-item">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Значение" />
+                                <span className="input-group-btn">
+                                  <button className="btn btn-info" type="button">Добавить</button>
+                                </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        );
     }
 });
