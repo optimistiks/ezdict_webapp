a2e71d0147ffe0d1ffd261436eabc026ea51d9c4
optimistiks <mail@potapovmax.com>
2015-11-05 20:26:06 +0300

    add release script

--- release.sh
+++ release.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-echo $1
-echo $2
+echo "release version is $1"
+echo "api client version is $2"
+echo "tag message is $3"
 
 git checkout -b release/$1
+sed -i -e 's/"version": "[[:digit:]].[[:digit:]].[[:digit:]]"/"version": "'$1'"/' package.json
+sed -i -e 's/optimistiks\/ezdict_api_client#.*"/optimistiks\/ezdict_api_client#'$2'"/' package.json
+git commit -a -m "bump version"
 
-git tag -a $1 -m "$2"
+git tag -a $1 -m "$3"
+
+git push --set-upstream origin release/$1
+git push --tags
